03dadc17548be00c4890f6f727f8c30aed2da82b
lusterhp <lusterhp@gmail.com>
2021-07-20 20:13:28 +0700

    push keenam
    sudah sampai menambahkan table pertama (table pesanan) di database

--- Website_TA/order/models.py
+++ Website_TA/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pesanan(models.Model):
+    kebutuhan_mesin = models.CharField(max_length=20)
+    kebutuhan_lapak = models.IntegerField(null=True)
+    kebutuhan_group = models.IntegerField(null=True)
+    waktu_mulai = models.DateTimeField()
+
+    def __str__(self):
+        return "{}".format(self.waktu_mulai)
